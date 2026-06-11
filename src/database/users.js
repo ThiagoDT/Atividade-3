342ea18c37ac9f00fa6d5b9ea5b34c5864204e85
ThiagoDT <galaspion5td@gmail.com>
2026-06-11 12:02:24 -0300

    test - alterando para conexão

--- src/database/users.js
+++ src/database/users.js
@@ -1,39 +1,24 @@
 const pool = require("./connection");
 
 async function listUsers(){
-    try{
-        const sql = "select * From users";
-            //manda o comando sql 
-        const {rows} = await pool.query(sql);
-        return rows;
-    }catch(error){
-        console.log(error.message)
-        return error;
-    }
+   const sql = "select * From users";
+    //manda o comando sql 
+   const {rows} = await pool.query(sql);
+   return rows;
 }
 
 async function createUser(name, email) {
-    try{
-        const sql = `insert into users(name,email) values ($1, $2) returning *`;
-        const values = [name, email];
-        const {rows} = await pool.query(sql,values);
-        return rows[0];
-    }catch(error){
-        return error.message;
-    }
-    
+    const sql = `insert into users(name,email) values ($1, $2) returning *`;
+    const values = [name, email];
+    const {rows} = await pool.query(sql,values);
+    return rows[0];
 }
 
 async function deleteUser(id) {
-    try{
-        const sql = `delete from users where id_user = $1 returning *`;
-        const values = [id];
-        const user = await pool.query(sql,values);
-        return user;
-    }catch(error){
-        return error.message;
-    }
-    
+    const sql = `delete from users where id_user = $1 returning *`;
+    const values = [id];
+    const user = await pool.query(sql,values);
+    return user;
 }
 
 module.exports={
