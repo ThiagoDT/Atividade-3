1578aae148ba1718470d0ff09720409f158270c4
ThiagoDT <galaspion5td@gmail.com>
2026-06-11 12:47:15 -0300

    retornando conexão

--- src/database/connection.js
+++ src/database/connection.js
@@ -8,7 +8,11 @@
 const{Pool} = require("pg");
 let connection;
 //configuração com o banco  de dados
-
+if(process.env.DATABASE_URL!=""){
+    connection={
+        connectionString: process.env.DATABASE_URL,
+    };
+}else{
     connection = {
     host:process.env.POSTGRES_HOST,
     user:process.env.POSTGRES_USER,
@@ -16,6 +20,7 @@ let connection;
     database:process.env.POSTGRES_DATABASE,
     port:process.env.POSTGRES_PORT
     }; 
+}
 
 
 //gerencia a conexão com o banco de dados
